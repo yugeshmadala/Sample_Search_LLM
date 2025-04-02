d355ee55d303d081765d1d85ebaa95acbc9c54fb
yugeshmadala <yugeshyogi972@gmail.com>
2025-04-02 10:46:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if prompt:=st.chat_input(placeholder="What is machine learning?"):
     llm=ChatGroq(groq_api_key=api_key,model_name="Llama3-8b-8192",streaming=True)
     tools=[search,arxiv,wiki]
 
-    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handling_parsing_errors=True)
+    search_agent=initialize_agent(tools,llm,agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,handle_parsing_errors=True)
 
     with st.chat_message("assistant"):
         st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
